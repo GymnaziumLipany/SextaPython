52d85f9818f91be2de25bd9ec6ef6dd6f9f1554a
Plasmoxy <sebohacker@gmail.com>
2018-03-31 13:46:23 +0200

    ocd

--- plasmoxy/hello.py
+++ plasmoxy/hello.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Canvas
 
 active = True
 x, y = 0, 0
@@ -6,12 +6,14 @@ x, y = 0, 0
 canvas = Canvas()
 canvas.pack()
 
-def motion(event):
-    global x,y
-    x, y = event.x, event.y
-    print('{}, {}'.format(x,y))
 
-canvas.bind('<B1-Motion>', motion);
+def motion(event):
+    global x, y
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+
+
+canvas.bind('<B1-Motion>', motion)
 
 while active:
     canvas.create_oval(x-10, y-10, x+10, y+10)
